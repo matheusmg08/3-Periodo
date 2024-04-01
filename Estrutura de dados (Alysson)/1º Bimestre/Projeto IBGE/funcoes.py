101524cb7b781b05ae37c6ff92ce510dd657a7d3
Matheus <129191137+matheusmg08@users.noreply.github.com>
2024-04-01 19:25:09 -0300

    Add files via upload

---
+++ Estrutura de dados (Alysson)/1º Bimestre/Projeto IBGE/funcoes.py
@@ -0,0 +1,44 @@
+import requests
+import unicodedata
+
+#função para remover os acentos
+def remover_acentos(texto):
+    return ''.join(c for c in unicodedata.normalize('NFD', texto)
+                   if unicodedata.category(c) != 'Mn')
+
+#função para gerar o json de todas cidades
+def lista_cidades():
+    url = "https://servicodados.ibge.gov.br/api/v1/localidades/distritos"
+    resposta = requests.get(url)
+    return resposta.json()
+
+#funçao para gerar o json de todas cidades do estado
+def busca_estado(estado):
+    url = f"https://servicodados.ibge.gov.br/api/v1/localidades/estados/{estado}/distritos"
+    resposta = requests.get(url)
+    #retorna a lista em json
+    return resposta.json()
+
+#função para criar a lista de cidades e tirar acentuação
+def nomes_cidades(lista):
+    cidades = []
+    for cidade in lista:
+        #nome recebe o nome da cidade, se não tiver nada recebe None
+        nome = cidade.get("nome", None)
+        #verifica so a variavel nome nao esta vazia, se nao estiver adiciona o nome no array teste
+        if nome != None:
+            cidades.append(nome)
+    #cria a lista sem acentuação   
+    cidades_sem_acentos= [remover_acentos(verificar) for verificar in cidades]
+        
+    return cidades_sem_acentos
+
+#função para ordenar a lista por ordem alfabetica
+def ordernar_cidades(json):
+    #chama a funcao nomes_cidade
+    lista = nomes_cidades(json)
+    #organiza por ordem alfabetica
+    lista.sort
+    ordenada = sorted(lista)
+
+    return ordenada
---
+++ Estrutura de dados (Alysson)/1º Bimestre/Projeto IBGE/ibge.py
@@ -0,0 +1,28 @@
+from flask import Flask,request
+from funcoes import lista_cidades, ordernar_cidades, busca_estado
+
+app = Flask(__name__)
+
+#Rota para buscar todas cidades do pais por ordem alfabetica
+@app.route("/distritos_ordenados")
+def distrito():
+    try:
+        json = lista_cidades()
+        resposta = ordernar_cidades(json)
+        return resposta
+    except Exception as e:
+        return f"Falha na rota /distritos_ordenados: {e}"
+
+#rota para mostrar todas cidades do estado em ordem alfabetica
+#necessario passar a sigla do estado como parametro "?estado=mg"   
+@app.route("/cidades_do_estado")
+def estados():
+    try:
+        estado = request.args.get("estado")
+        json = busca_estado(estado)
+        resposta = ordernar_cidades(json)
+        return resposta
+    except Exception as e:
+        return f"Falha na rota /cidades_do_estado: {e}"
+    
+app.run(debug=True)
